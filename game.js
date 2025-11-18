4fd6ad44e1048e859f0bf52df689f6d79a4cbf60
Forkentiney <jseverud@proton.me>
2025-11-18 14:00:41 -0600

    Started writing pseudocode.

---
+++ game.js
@@ -0,0 +1,11 @@
+// 1. Generate a random number between 0 and 1.
+// 2. If number is less than 0.33, return rock.
+// 3. If number is greater than 0.33 but less than 0.66, return paper.
+// 4. If number is greater than 0.66, return scissors.
+//
+// 5. Prompt user for either rock, paper, or scissors.
+// 6. If inputs are the same, tie 
+// 7. If player uses paper and computer uses rock, player wins. Else, computer wins.
+// 8. If player uses rock and computer uses scissors, player wins. Else, computer wins.
+// 9. IF player uses scissors and computer uses paper, player wins. Else, computer wins.
+//
